a2a44b1ea508ac56322f5a39443315f0db870482
Mitchell Opitz <mitchell@mitchellopitz.net>
2026-05-04 23:25:02 -0500

    fix - add cron_schedule filter manually inside opibluesky_activate

--- opi-bluesky/functions.php
+++ opi-bluesky/functions.php
@@ -24,6 +24,14 @@ register_activation_hook( __FILE__,   'opibluesky_activate' );
 register_deactivation_hook( __FILE__, 'opibluesky_deactivate' );
 
 function opibluesky_activate(): void {
+    add_filter( 'cron_schedules', function( $schedules ) {
+        $schedules['opi_bluesky_1min'] = [
+            'interval' => 60,
+            'display'  => __( 'Every Minute', 'opi-bluesky' ),
+        ];
+        return $schedules;
+    } );
+
     if ( ! wp_next_scheduled( 'opi_bluesky_process' ) ) {
         wp_schedule_event( time(), 'opi_bluesky_1min', 'opi_bluesky_process' );
     }
